92a562f77d189e0b23f51a721219fb7b83e9263c
Ismll <ismll.acc@mail.ru>
2024-05-28 12:55:23 +0400

    created model, register in admin, set up the views

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
