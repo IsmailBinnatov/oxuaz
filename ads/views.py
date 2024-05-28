edbf0e7dd504acbcebec2d210a7ca7bdd777a38c
Ismll <ismll.acc@mail.ru>
2024-05-28 13:08:01 +0400

    set up views and index.html, the main page is ready

--- ads/views.py
+++ ads/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Feed
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'ads/index.html')
+    feeds = Feed.objects.all()
+    context = {'feeds': feeds}
+    return render(request, 'ads/index.html', context)
